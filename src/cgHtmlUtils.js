4b7891f908c96d27da5c9633462e19d672fc91e8
cgil <lao.tseu.is.alive@gmail.com>
2019-05-13 10:22:05 +0200

    added getParams to analyse url params

--- src/cgHtmlUtils.js
+++ src/cgHtmlUtils.js
@@ -142,3 +142,22 @@ export const findByKeyValue = function (arrToSearch, keyName, keyValue) {
   }
   return null;
 }
+
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ * @param  {String} url The URL
+ * @return {Object}     The URL parameters
+ */
+export const getParams = function (url) {
+	let params = {};
+	let parser = document.createElement('a');
+	parser.href = url;
+	const query = parser.search.substring(1);
+	const vars = query.split('&');
+	for (let i = 0; i < vars.length; i++) {
+		const pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
